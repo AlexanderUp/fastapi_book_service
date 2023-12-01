afdadc90d73a83e604458d0b3ba8af806817e879
AlexanderUp <alexander.uperenk0@yandex.ru>
2023-12-01 23:41:52 +0300

    DB dependency

--- database.py
+++ database.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./db.sqlite/'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+)
 
 SessionLocal = sessionmaker(bind=engine)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from database import SessionLocal
 from domain.book_service import BookService
@@ -7,37 +7,50 @@ from domain.schemas import AuthorSchema, BookSchema, BookSchemaCreate, Publisher
 
 app = FastAPI()
 
-session = SessionLocal()
 
-book_repository = SQLBookRepository(session)
-book_service = BookService(book_repository)
+def get_service():
+    session = SessionLocal()
+    book_repository = SQLBookRepository(session)
+    book_service = BookService(book_repository)
+    
+    try:
+        yield book_service
+    finally:
+        session.close()
+
 
 
 @app.get('/books', response_model=list[BookSchema])
-async def list_books():
+async def list_books(book_service: BookService = Depends(get_service)):
     return book_service.list_books()
 
 
 @app.post('/books', response_model=BookSchema)
-async def add_book(book: BookSchemaCreate):
+async def add_book(
+    book: BookSchemaCreate, book_service: BookService = Depends(get_service),
+):
     return book_service.add_book(book)
 
 
 @app.get('/authors', response_model=list[AuthorSchema])
-async def list_authors():
+async def list_authors(book_service: BookService = Depends(get_service)):
     return book_service.list_authors()
 
 
 @app.post('/authors', response_model=AuthorSchema)
-async def add_author(author: AuthorSchema):
+async def add_author(
+    author: AuthorSchema, book_service: BookService = Depends(get_service),
+):
     return book_service.add_author(author)
 
 
 @app.get('/publishers', response_model=list[PublisherSchema])
-async def list_publishers():
+async def list_publishers(book_service: BookService = Depends(get_service)):
     return book_service.list_publishers()
 
 
 @app.post('/publishers', response_model=PublisherSchema)
-async def add_publisher(publisher: PublisherSchema):
+async def add_publisher(
+    publisher: PublisherSchema, book_service: BookService = Depends(get_service),
+):
     return book_service.add_publisher(publisher)
